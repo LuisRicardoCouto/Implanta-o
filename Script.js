60868f231d57303487514da1c8203a4aec8239d5
LuisRicardoCouto <ricardocouto459@gmail.com>
2025-05-27 09:43:35 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+  // Simulate loading
+  document.addEventListener('DOMContentLoaded', function() {
+    const progressBars = document.querySelectorAll('.progress-fill');
+    
+    progressBars.forEach(bar => {
+        const width = bar.style.width;
+        bar.style.width = '0%';
+        
+        setTimeout(() => {
+            bar.style.width = width;
+        }, 100);
+    });
+});
